3fa23b605d2e875e5e234be470fb64b1e9651a12
Faraz <farazrze18@gmail.com>
2024-03-10 14:16:39 +0330

    blog models
    blog models was completed

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
