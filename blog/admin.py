1ec1486424006f6ac92edb721910122c32380f20
Faraz <farazrze18@gmail.com>
2024-03-11 12:25:05 +0330

    model admin objects
    some model admin object were added to blog/admin.py to Class Postadmin().

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from blog.models import post
 
-admin.site.register (post)
+
+class Postadmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = 'empty'
+    #fields = ('title','content')
+    #exclude = ('status',)
+    list_display = ('id','title','created_date')
+    list_filter = ('status','created_date')
+    search_fields = ('title','content')
+    ordering = ['-created_date']
+
+
+
+
+admin.site.register (post,Postadmin)
