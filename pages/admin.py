8469ebd1e32787dbfdbc1169b01bb98bd94ca5a1
Faraz <farazrze18@gmail.com>
2024-03-11 14:58:11 +0330

    contact model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from pages.models import Contact
 
-# Register your models here.
+
+
+
+class Contactadmin(admin.ModelAdmin):
+    list_display = ('name','email','subject','created_date')
+    list_filter = ('name','email')
+    search_fields = ['name','subject','email','message']
+
+
+
+
+admin.site.register(Contact,Contactadmin)
--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
@@ -8,3 +9,12 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_date']
+        verbose_name = 'مخاطب'
+        verbose_name_plural = 'مخاطبین'
+
+    def __str__(self):
+        return self.name
+    
