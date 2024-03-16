b481399ed4d0e7309e980f401af119884914c93b
Faraz <farazrze18@gmail.com>
2024-03-16 11:22:01 +0330

    create imgefield

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
