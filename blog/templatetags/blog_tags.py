3a87cba448101fbe3d9ebf6058a5810f21e6a4c8
Faraz <farazrze18@gmail.com>
2024-03-19 21:37:30 +0330

    filter tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,5 +4,13 @@ register = template.Library()
 
 @register.simple_tag(name='post')
 def test():
-    posts = Post.objects.filter(status=1).count()
+    posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg]
+
+
+
+
